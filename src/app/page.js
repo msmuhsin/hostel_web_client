d588a8aa454ec93b1b0d590ed76df4ad1417075c
Abdul Majid <majid616365@gmail.com>
2024-04-22 15:04:53 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function App() {
-  return <h1 className="text-lg font-semibold md:text-2xl">Dashboard</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-y-5 bg-slate-950">
+      <Link href="/dashboard">
+        <Button>Go to Dashboard</Button>
+      </Link>
+    </div>
+  );
 }
